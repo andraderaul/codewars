a90fcafa16de32c61a5c20fb4fe42d7715992fe6
Raul Andrade <raul.andrade@mercadofavo.com>
2022-11-08 23:06:26 -0300

    feat: add digital root solution

---
+++ src/bin/digital_root.rs
@@ -0,0 +1,25 @@
+/*
+export const digitalRoot = (n:number):number => {
+  return (n - 1) % 9 + 1
+};
+*/
+
+fn digital_root(n: i64) -> i64 {
+    (n - 1) % 9 + 1
+}
+
+fn main() {
+    let x = digital_root(16);
+
+    println!("{}", x)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::digital_root;
+
+    #[test]
+    fn returns_expected() {
+        assert_eq!(digital_root(16), 7);
+    }
+}
