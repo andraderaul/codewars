0e41f53c7685381fdcb9a0d5d1e6f01f41713d84
Raul Andrade <raul.andrade@mercadofavo.com>
2022-11-27 10:14:52 -0300

    feat: pyramid array

---
+++ src/bin/pyramid_array.rs
@@ -0,0 +1,20 @@
+fn pyramid(n: usize) -> Vec<Vec<u32>> {
+    (1..=n).map(|i| vec![1; i]).collect()
+}
+
+fn main() {
+    println!("{:?}", pyramid(3));
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn basic() {
+        assert_eq!(pyramid(0), vec![] as Vec<Vec<u32>>);
+        assert_eq!(pyramid(1), vec![vec![1]]);
+        assert_eq!(pyramid(2), vec![vec![1], vec![1, 1]]);
+        assert_eq!(pyramid(3), vec![vec![1], vec![1, 1], vec![1, 1, 1]]);
+    }
+}
