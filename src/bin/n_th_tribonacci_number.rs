d1e1c7a6e50f9891c095771801b7ab8754d98e56
Raul Andrade <raul.andrade@mercadofavo.com>
2022-11-29 09:45:21 -0300

    feat: n th tribonacci number

---
+++ src/bin/n_th_tribonacci_number.rs
@@ -0,0 +1,27 @@
+/* https://leetcode.com/problems/n-th-tribonacci-number/description/ */
+
+fn tribonacci(n: i32) -> i32 {
+    let mut memo = vec![0; n as usize + 3];
+    memo[0] = 0;
+    memo[1] = 1;
+    memo[2] = 1;
+
+    for i in 3..=n as usize {
+        memo[i] = memo[i - 1] + memo[i - 2] + memo[i - 3];
+    }
+
+    memo[n as usize]
+}
+
+fn main() {}
+
+#[cfg(test)]
+mod tests {
+    use super::tribonacci;
+
+    #[test]
+    fn basic() {
+        assert_eq!(tribonacci(4), 4);
+        assert_eq!(tribonacci(25), 1389537)
+    }
+}
