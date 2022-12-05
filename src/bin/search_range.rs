99029bce944dff8cfe3d698282499d1a9a28d047
Raul Andrade <raul.andrade@mercadofavo.com>
2022-12-05 10:16:34 -0300

    refactor: search range

--- src/bin/search_range.rs
+++ src/bin/search_range.rs
@@ -1,3 +1,5 @@
+/* https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/description/ */
+
 pub fn search_range(nums: Vec<i32>, target: i32) -> Vec<i32> {
     let i = nums.partition_point(|n| n < &target);
 
