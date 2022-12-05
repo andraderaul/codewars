7b9529836fb48374d9910d1f589d2fa8f79571aa
Raul Andrade <raul.andrade@mercadofavo.com>
2022-12-05 11:28:06 -0300

    refactor: contain all roots

--- src/bin/contain_all_rots.rs
+++ src/bin/contain_all_rots.rs
@@ -1,3 +1,5 @@
+/* https://www.codewars.com/kata/5700c9acc1555755be00027e/train/rust */
+
 fn contain_all_rots(strng: &str, arr: Vec<&str>) -> bool {
     (0..strng.len())
         .map(|index| strng[index..].to_owned() + &strng[..index])
