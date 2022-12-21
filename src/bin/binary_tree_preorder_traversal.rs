323ace660b28650ef3e28d575363b8eb147cf047
Raul Andrade <raul.andrade@mercadofavo.com>
2022-12-21 11:42:02 -0300

    feat: preorder traversal

---
+++ src/bin/binary_tree_preorder_traversal.rs
@@ -0,0 +1,41 @@
+/* https://leetcode.com/problems/binary-tree-preorder-traversal/description/ */
+
+use std::cell::RefCell;
+use std::rc::Rc;
+
+// Definition for a binary tree node.
+#[derive(Debug, PartialEq, Eq)]
+pub struct TreeNode {
+    pub val: i32,
+    pub left: Option<Rc<RefCell<TreeNode>>>,
+    pub right: Option<Rc<RefCell<TreeNode>>>,
+}
+impl TreeNode {
+    #[inline]
+    pub fn new(val: i32) -> Self {
+        TreeNode {
+            val,
+            left: None,
+            right: None,
+        }
+    }
+}
+
+pub fn preorder_traversal(mut root: Option<Rc<RefCell<TreeNode>>>) -> Vec<i32> {
+    let mut answer: Vec<i32> = vec![];
+
+    fn walk(node: &mut Option<Rc<RefCell<TreeNode>>>, ans: &mut Vec<i32>) {
+        if let Some(n) = node {
+            let mut n = n.borrow_mut();
+            ans.push(n.val);
+            walk(&mut n.left, ans);
+            walk(&mut n.right, ans);
+        }
+    }
+
+    walk(&mut root, &mut answer);
+
+    answer
+}
+
+fn main() {}
