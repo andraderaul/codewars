a3d710d7c36481bf129d22ea0d7f72f815967e94
Raul Andrade <raul.andrade@lend.tech>
2023-01-25 11:06:38 -0300

    tests: add test to unique in order

--- codewars-py/codewars_py/src/unique_in_order.py
+++ codewars-py/codewars_py/src/unique_in_order.py
@@ -1,4 +1,9 @@
-def unique_in_order(sequence):
+# https://www.codewars.com/kata/54e6533c92449cc251001667/train/python
+
+from typing import Union, List
+
+
+def unique_in_order(sequence: List[Union[str, int]]):
     res = []
     for item in sequence:
         if len(res) == 0 or item != res[-1]:
---
+++ codewars-py/tests/src/test_unique_in_order.py
@@ -0,0 +1,33 @@
+from codewars_py.src.unique_in_order import unique_in_order
+
+import pytest
+
+
+def assert_equals(expected, real):
+    assert expected == real
+
+
+def test_should_work_with_empty_sequence():
+    assert_equals(unique_in_order(""), [])
+    assert_equals(unique_in_order([]), [])
+    assert_equals(unique_in_order(()), [])
+
+
+def test_should_work_with_single_element_sequence():
+    assert_equals(unique_in_order("A"), ["A"])
+    assert_equals(unique_in_order(["A"]), ["A"])
+    assert_equals(unique_in_order(("A",)), ["A"])
+
+
+def test_should_reduce_duplicates():
+    assert_equals(unique_in_order("AA"), ["A"])
+    assert_equals(unique_in_order("AAAABBBCCDAABBB"), ["A", "B", "C", "D", "A", "B"])
+
+
+def should_be_case_sensitive():
+    assert_equals(unique_in_order("ABBCcA"), ["A", "B", "C", "c", "A"])
+
+
+def should_work_with_different_element_types():
+    assert_equals(unique_in_order([1, 2, 3, 3, -1]), [1, 2, 3, -1])
+    assert_equals(unique_in_order(["a", "b", "b", "a"]), ["a", "b", "a"])
