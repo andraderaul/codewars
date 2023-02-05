a2b476dd1aaac5b481428d208ea6630b2da8a6ea
Raul Andrade <raul.andrade@lend.tech>
2023-02-04 22:03:35 -0300

    refactor: add typing to cakes

--- codewars-py/codewars_py/src/cakes.py
+++ codewars-py/codewars_py/src/cakes.py
@@ -1,4 +1,7 @@
-def cakes(recipe, available):
+from typing import Dict
+
+
+def cakes(recipe: Dict[str, int], available: Dict[str, int]):
     return min(
         [available[item] // recipe[item] if item in available else 0 for item in recipe]
     )
